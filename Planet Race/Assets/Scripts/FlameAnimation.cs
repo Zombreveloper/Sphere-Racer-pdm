dc77e6815754b20e0350f50d9963ee29c309bc33
Marvin <ma.stickan@gmail.com>
2021-03-01 17:59:56 +0100

    Spaceship now with poorly animated Flames
    Ich bin wirklich nicht mehr motiviert genug um irgendwas zu machen was über ein bisschen Wobbeln von schlecht texturierten Regentropfen hinausgeht

---
+++ Planet Race/Assets/Scripts/FlameAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameAnimation : MonoBehaviour
+{
+	//private GameObject spaceFlame;
+	float XRandom, YRandom, ZRandom;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        //spaceFlame = this.GameObject;
+		
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		XRandom = Random.Range(0.1416146f, 0.2f);
+		YRandom = Random.Range(0.7f, 0.8f);
+		ZRandom = Random.Range(0.1f, 0.2f);
+		
+       transform.localScale = new Vector3 (XRandom, YRandom, ZRandom); 
+    }
+}
