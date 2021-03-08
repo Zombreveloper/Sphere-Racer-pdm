14e0102fdc963919afc4bb2bfeda0b4fed0bb339
Lazer <98yoda@gmx.de>
2021-03-08 17:09:42 +0100

    Pfeil Upgrade
    Pfeil ist weiter oben, und Zeigt quasie ueber den Checkpoint

--- Planet Race/Assets/Scripts/DirectionArrow.cs
+++ Planet Race/Assets/Scripts/DirectionArrow.cs
@@ -3,37 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DirectionArrow : MonoBehaviour {
-	
+
 	[SerializeField]
 	private Transform target;
 	public Transform RaceManager;
     private RaceManager _manager;
 
-	
+	public TinyPlanetGravitation myPlanet; //Gravitationsberechnung für vector und positions zeug
+	public GameObject playerCar;//car that you want to follow
+
+
     void Awake()
     {
         _manager = GameObject.FindObjectOfType<RaceManager>();
-     
     }
-	
-	
-	private void Update() 
+
+	void Start()
+    {
+        playerCar = GameObject.Find("currentCar");
+		myPlanet = playerCar.GetComponent<TinyPlanetGravitation>(); //tinyplanet vom car getten
+    }
+
+	private void Update()
 	{
-		
-		
+		Vector3 upDirection = Vector3.Normalize(myPlanet.directionUp);
+
 		target = _manager.checkPointMesh; // Position des nächsten Checkpoints
-		
-		
+
 		// Rotation auf diesen.
-		Vector3 targetPosition = target.position - transform.position;
+		Vector3 targetPosition = (target.position + (upDirection*3)) - transform.position;//guckt ueber checkpoint Position
 		Quaternion rotation = Quaternion.LookRotation(targetPosition);
 		transform.rotation = rotation;
-		
 	}
-	
-	
-	
-	
-	
-	
 }
